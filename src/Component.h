03d8c527bd54c821845a13890c4de1ee471c3ebc
Martin Ohlsson <90261294+MMAAOOHH@users.noreply.github.com>
2022-10-11 14:19:45 +0200

    Entity.h and component struct

--- src/Component.h
+++ src/Component.h
@@ -1,4 +1,6 @@
 #pragma once
 
-class IComponent
-{};
+struct Component
+{
+	virtual ~Component() = default;
+};
--- src/Entity.h
+++ src/Entity.h
@@ -1,3 +1,119 @@
 #pragma once
 
 #include "Component.h"
+#include <glad/glad.h>
+#include <iostream>
+#include <unordered_map> // 
+#include "Splay_tree.h"
+
+using component_id = std::size_t;
+
+inline component_id get_new_component_type_id()
+{
+	static component_id last_id = 0u;
+	return ++last_id;
+}
+
+// depending on how many components there are creates a seperate get_component_type_if for each, thus giving the illusion of unique id
+template <typename T> inline component_id get_component_type_id() noexcept
+{
+	// assert that it inherits Comp
+	static_assert (std::is_base_of<Component, T>::value, "");
+	static component_id type_id = get_new_component_type_id();
+	return type_id;
+}
+
+
+class Entity
+{
+private:
+	SplayTree<Component> components_;
+	SplayTree<Entity> children_;
+public:
+	Entity()
+		: components_(), children_()
+	{}
+	~Entity(); 
+
+	Entity* get_child(GLuint pos)
+	{
+		return children_.search(pos);
+	}
+
+	Entity* get_child(std::string str)
+	{
+		return children_.search((int)std::hash<std::string>{}(str));
+	}
+
+	std::vector<Entity*> get_child_list()
+	{
+		return children_.get_ordered_list();
+	}
+
+	bool has_child(std::string str)
+	{
+		return get_child(str);
+	}
+
+	GLuint children_size()
+	{
+		return children_.size();
+	}
+
+	// inserts entity into child tree, and treats it like an array
+	Entity* push_back_child(Entity* entity)
+	{
+		return children_.insert(entity);
+	}
+
+	// inserts entity into child tree with hash of str as unique id
+	Entity* add_child(Entity* entity, std::string str)
+	{
+		return children_.insert(std::hash<std::string>{}(str), entity);
+	}
+
+	// gets component of matching unique id, throws if not there
+	template<typename T> T* get_component()
+	{
+		return static_cast<T*>(components_.search(get_component_type_id<T>()));
+	}
+
+	// searches component tree for templated component at pos
+	template<typename T> T* get_component(int pos)
+	{
+		return static_cast<T*>(components_.search(pos));
+	}
+
+	// checks if unique id of component is located in component tree
+	template <typename T> bool has_component()
+	{
+		return components_.search(get_component_type_id<T>());
+	}
+
+	// adds component to splay tree and gives it a uniquely id, can't have two of the same component per entity
+	template <typename T, typename... TArgs> T* add_component(TArgs&&... m_args)
+	{
+		T* c(new T(std::forward<TArgs>(m_args)...));
+
+		return static_cast<T*>(components_.insert(get_component_type_id<T>(), c));
+	}
+
+	// adds component to splay tree and treats it like an array, can have two of the same component in an entity
+	template <typename T, typename... TArgs> T* push_back_component(TArgs&&... m_args)
+	{
+		T* c(new T(std::forward<TArgs>(m_args)...));
+
+		return static_cast<T*>(components_.insert(c));
+	}
+
+	// gets array of components
+	std::vector<Component*> get_component_list()
+	{
+		return components_.get_ordered_list();
+	}
+
+	GLuint component_size()
+	{
+		return components_.size();
+	}
+};
